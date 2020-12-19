cf5bbe753c4e3bde9b9d446cdfe5937497ffd946
Laxman Bista <bistalaxman5@gmail.com>
2020-12-18 21:10:20 -0600

    Additional Files

---
+++ src/Contact.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+// import Form from './Form';
+// import './ContactStyle.css';
+
+
+// const Contact  = () => {
+//     return (
+//         <div className = 'contactstyle'>
+//             <h2> Welcome to Contact Page </h2>
+//             <Form/>
+//         </div>
+//     )
+// }
